79309374f9c346b1050a08d2526319ac133f7ae4
schuckbeta <passionchenjianye@gmail.com>
2018-01-13 17:34:00 +0800

    Add API GateWay Zuul

---
+++ api-zuul/src/main/java/com/cjy/platform/zuul/ApiZuulApplication.java
@@ -0,0 +1,14 @@
+package com.cjy.platform.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ApiZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiZuulApplication.class, args);
+	}
+}
